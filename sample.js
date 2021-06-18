543a26c6a3b0416bf99dcb860a9eb8ea9901924c
maul1998 <68526806+maul1998@users.noreply.github.com>
2021-06-18 10:24:56 +0700

    Delete sample.js
    Delete

--- sample.js
+++
@@ -1,9 +0,0 @@
-function changeAnchor() {
-  document.write(document.cookie);
-  document.getElementById('myAnchor').href=document.cookie;
-}
-
-function fiddleHistory() {
-	window.history.go(document.location);
-	window.history.next = document.location;
-}
